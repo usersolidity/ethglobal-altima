fe379b77ed834a17a4cf0f747a80a850146820bd
Jheave <jimenezjheave123@gmail.com>
2022-02-04 20:07:14 +0800

    delete greeter

--- test/test.js
+++ test/test.js
@@ -1,10 +1,10 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
+describe("Altima", function () {
   it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
+    const altimaContract = await ethers.getContractFactory("Altima");
+    const greeter = await altimaContract.deploy("Hello, world!");
     await greeter.deployed();
 
     expect(await greeter.greet()).to.equal("Hello, world!");
